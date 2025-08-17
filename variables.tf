567a52fe9660489160d67c97110237b57193fdca
Dhondiba7070 <dhondibakoli7070@gmail.com>
2025-08-17 12:28:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
-# No SSH key needed since we removed remote_access
-# Keeping variable in case you want to re-add later
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for worker nodes (optional)"
+  description = "The name of the SSH key pair to use for EC2 nodes"
   type        = string
-  default     = ""
+  default     = "KEY"
 }
