7e138ad9e58d17be6824a60ca7f22fe0d5e36aba
Dhondiba7070 <dhondibakoli7070@gmail.com>
2025-08-30 17:29:54 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,6 @@
-# AWS region variable
-variable "region" {
-  description = "The AWS region where resources will be created"
-  type        = string
-  default     = "us-east-1"  # Set default region, can be overridden
-}
-
-# SSH Key Name variable
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for EC2 nodes. Ensure the key pair exists in the selected AWS region."
   type        = string
-  default     = ""  # No default, forces the user to specify a value
 
   validation {
     condition     = length(var.ssh_key_name) > 0
