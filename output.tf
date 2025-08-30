2b1a3db66d5132e0233c822531886b1309f821ec
Dhondiba7070 <dhondibakoli7070@gmail.com>
2025-08-30 17:16:02 +0530

    output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "cluster_id" {
-  value = aws_eks_cluster.dhondiba.id
+  description = "The ID of the EKS Cluster"
+  value       = aws_eks_cluster.dhondiba.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.dhondiba.id
+  description = "The ID of the EKS Node Group"
+  value       = aws_eks_node_group.dhondiba.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.dhondiba_vpc.id
+  description = "The ID of the VPC where EKS is deployed"
+  value       = aws_vpc.dhondiba_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.dhondiba_subnet[*].id
+  description = "The IDs of the subnets used for the EKS Cluster"
+  value       = aws_subnet.dhondiba_subnet[*].id
 }
