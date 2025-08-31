e2bbd1702c434e45e911d0392e60b097d4fe7fa6
Dhondiba7070 <dhondibakoli7070@gmail.com>
2025-08-31 13:55:44 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,5 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for EC2 nodes. Ensure the key pair exists in the selected AWS region."
+  description = "The name of the SSH key pair to use for instances"
   type        = string
-
-  validation {
-    condition     = length(var.ssh_key_name) > 0
-    error_message = "You must specify a non-empty SSH key name."
-  }
+  default     = "KEY"
 }
